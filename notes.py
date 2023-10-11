1849a933a8951d72ed9b2a60b0e2856c27257601
Amanda Xiao <amanda64@email.unc.edu>
2023-10-11 14:26:15 -0400

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,2 +1,7 @@
 # Use this file for notes and running examples...
 # As expected, run it with `python3 -m notes`
+
+from sqlalchemy import create_engine
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+print(engine)
